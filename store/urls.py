923abbfbf77bac1a96416cb4a0755a6f9e629ad4
chominha <132678309+chominha@users.noreply.github.com>
2023-06-19 20:14:54 +0900

    사진과 설명, 가격 보임

--- store/urls.py
+++ store/urls.py
@@ -6,7 +6,7 @@ from .views.login import Login , logout
 from .views.cart import Cart
 from .views.checkout import CheckOut
 from .views.orders import OrderView
-from .views.detail import detail
+from .views.detail import product_detail
 from .middlewares.auth import  auth_middleware
 
 
@@ -20,5 +20,5 @@ urlpatterns = [
     path('cart', auth_middleware(Cart.as_view()) , name='cart'),
     path('check-out', CheckOut.as_view() , name='checkout'),
     path('orders', auth_middleware(OrderView.as_view()), name='orders'),
-    path('detail', detail, name='detail' ),
+    path('detail/<int:product_id>/', product_detail, name='detail'),
 ]
--- store/views/detail.py
+++ store/views/detail.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render , redirect , HttpResponseRedirect
+from django.shortcuts import render , redirect , HttpResponseRedirect, get_object_or_404
 from store.models.product import Product
 from store.models.category import Category
 from django.views import View
 
-def detail(request):
-    return render(request, 'detail.html')
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    context = {'product': product}
+    return render(request, 'detail.html', context)
